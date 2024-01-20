ea8e295f97d20ce2d2a8cb45a7441ad2d34e7db6
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2024-01-21 02:36:55 +0700

    ok

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,1 @@
-std::vector<int> PolyHash::pow1{1};
-// std::vector<ull> PolyHash::pow2{1};
+sumDivisors
--- template.cpp
+++ template.cpp
@@ -1,4 +1,5 @@
 /**
+ * Author: Thinh Ngo Ngoc
  * Solution for: 
 */
 
@@ -10,46 +11,21 @@ using namespace std;
 using namespace chrono;
 using namespace __gnu_pbds;
 
-/* clang-format off */
-
 /* TYPES  */
 #define ll long long
 #define ull unsigned long long
-#define pii pair<int, int>
-#define pll pair<long long, long long>
-#define vi vector<int>
-#define vll vector<long long>
-#define mii map<int, int>
-#define si set<int>
-#define sc set<char>
-
-/* FUNCTIONS */
-#define f(i,s,e) for(long long int i=(s);i<(e);i++)
-#define cf(i,s,e) for(long long int i=(s);i<=(e);i++)
-#define rf(i,e,s) for(long long int i=(e);i>=(s);i--)
-#define fe(a, b) for (auto&(a) : (b))
-#define fi first
-#define se second
-#define mp make_pair
-#define pb push_back
-#define eb emplace_back
-
-/* PRINTS */
-template <class T>
-void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
-
-/* UTILS */
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-ll getRandomNumber(ll l, ll r) {return uniform_int_distribution<ll>(l, r)(rng);}
 typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
 
-#define PI 3.1415926535897932384626433832795
-double EPS = 1e-9;
+/* CONSTANTS  */
+const double PI = 3.1415926535897932384626433832795;
+const double EPS = 1e-9;
 int INF = 1000000005;
 long long INFF = 1000000000000000005LL;
 const long long MOD = 1000000007;
 vector<pair<ll,ll>> moves = {{-1,0},{0,1},{1,0},{0,-1},{-1,1},{1,1},{1,-1},{-1,-1}}; // UP-RIGHT-BOTTOM-DOWN || NORTH-EAST-SOUTH-WEST, From moves[4] Right of Up then clock wise
-#define read(type) readInt<type>()
+
+/* UTILS */
 ll min(ll a,int b) { if (a<b) return a; return b; }
 ll min(int a,ll b) { if (a<b) return a; return b; }
 ll max(ll a,int b) { if (a>b) return a; return b; }
@@ -61,15 +37,14 @@ string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a
 string int_to_string(ll a) { char x[100]; sprintf(x, "%lld", a); string s = x; return s; }
 ll string_to_int(string a) { char x[100]; ll res; strcpy(x, a.c_str()); sscanf(x, "%lld", &res); return res; }
 bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+ll getRandomNumber(ll l, ll r) { return uniform_int_distribution<ll>(l, r)(rng); } // get rand in [l, r)
 ll mod(ll x, ll _mod) { return (((x%_mod) + _mod) % _mod); }
 ll addMod(ll a, ll b, ll _mod) { return mod(mod(a, _mod) + mod(b, _mod), _mod); }
 ll subMod(ll a, ll b, ll _mod) { return mod(mod(a, _mod) - mod(b, _mod), _mod); }
 ll mulMod(ll a, ll b, ll _mod) { return mod(mod(a, _mod) * mod(b, _mod), _mod); }
 ll powMod(ll x, ll n, ll _mod) { if (n == 0) return 1%_mod; ll u = powMod(mod(x,_mod),n/2,_mod); u = (u*u)%_mod; if (n%2 == 1) u = (u*(mod(x,_mod)))%_mod; return u; } // (x^n)%_mod
-ll inverseMod(ll a, ll _mod) { if (gcd(a, _mod) != 1) {cout << "There is no inverse mod of " << a << " and " << _mod << "\n"; return -1;}; return powMod(mod(a,_mod), _mod-2, _mod); } // Return -1 if ((1/a)%_mod) not exist (a and _mod not coprime)
+ll inverseMod(ll a, ll _mod) { assert(gcd(a, _mod) == 1); return powMod(mod(a,_mod), _mod-2, _mod); } // Raise error if (a and _mod not coprime)
 ll divMod(ll a, ll b, ll _mod) { ll tmp = inverseMod(b, _mod); if (tmp == -1) {cout << "There is no divided mod of " << a << " and " << b << "\n"; return -1;} return (((mod(a, _mod))*inverseMod(mod(b, _mod), _mod))%_mod); } // Return -1 if ((1/b)%_mod) not exist (b and _mod not coprime)
-void yes() { cout<<"YES\n"; }
-void no() { cout<<"NO\n"; }
 #define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 void IN_OUT() {
 #ifndef ONLINE_JUDGE
@@ -84,12 +59,11 @@ typedef unsigned long int uint32;
 typedef long long int int64;
 typedef unsigned long long int  uint64;
 
-/* clang-format on */
+/* Main Program */
 void solve() {
 
 }
 
-/* Main()  function */
 int main() {
     IN_OUT();
     fastio();
@@ -104,4 +78,4 @@ int main() {
     return 0;
 }
 
-/* Main() Ends Here */
+/* Main Program End Here */
