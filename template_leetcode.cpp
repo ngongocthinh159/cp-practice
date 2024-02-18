bda54607fed11a1ad4197d9b7be4975e7a201bdb
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2024-02-18 14:38:46 +0700

    ok

---
+++ template_leetcode.cpp
@@ -0,0 +1,17 @@
+/**
+ * Author: Thinh Ngo Ngoc
+ * Solution for: 
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+
+int main() {
+    fastio();
+    
+    return 0;
+}
