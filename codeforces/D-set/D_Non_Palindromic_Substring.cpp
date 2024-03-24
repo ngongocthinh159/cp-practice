fcfb105682207e50d4e0066be15d01cbf8586489
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2024-03-24 19:57:32 +0700

    ok

---
+++ codeforces/D-set/D_Non_Palindromic_Substring.cpp
@@ -0,0 +1,229 @@
+/**
+ * Author: Thinh Ngo Ngoc
+ * Solution for: 
+*/
+#pragma GCC optimize("O3,unroll-loops")
+ 
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+void IN_OUT() {
+#ifndef ONLINE_JUDGE
+freopen("Input.txt", "r", stdin);
+freopen("Output.txt", "w", stdout);
+#endif
+}
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits(x) __builtin_popcountll(x)
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+// #define ThinhNgo
+#ifdef ThinhNgo
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+ 
+using ll = long long;
+using ull = unsigned long long;
+using lld = long double;
+typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+void _print(pbds v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+/*---------------------------------------------------------------------------------------------------------------------------*/
+ll __gcd__(ll a, ll b) {if (!a || !b) return a | b; unsigned shift = __builtin_ctz(a | b); a >>= __builtin_ctz(a); do { b >>= __builtin_ctz(b); if (a > b) swap(a, b); b -= a; } while (b); return a << shift;} // only a >= 0 && b >= 0
+ll gcd(ll a, ll b) {a = abs(a); b = abs(b); return __gcd__(a, b);} // get abs(a), abs(b) in case a < 0 || b < 0
+ll expo(ll a, ll b, ll mod) {if (a==0) return 1%mod; ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;} // a = 0 return 0 | b = 0 return 1
+ll expo(ll a, ll b) {ll res = 1; while (b > 0) {if (b & 1)res = res * a; a = a * a; b = b >> 1;} return res;} // a = 0 return 0 | b = 0 return 1
+void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
+ll mminvprime(ll a, ll b) {assert(gcd(a, b) == 1); return expo(a, b - 2, b);} // inverse mod: a^-1 % b = a^(b-2) % b. When gcd(a,b)=1 (or b prime)
+bool revsort(ll a, ll b) {return a > b;}
+ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+void google(int t) {cout << "Case #" << t << ": ";}
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+ll getRandomNumber(ll l, ll r) {return uniform_int_distribution<ll>(l, r)(rng);} 
+/*--------------------------------------------------------------------------------------------------------------------------*/
+#define ThinhNgo_use_cases
+
+void pre_compute() {
+
+}
+
+
+struct Manacher {
+    string s; // aabb
+    vector<int> P; // #a#a#b#b#
+    Manacher(string s) {
+        this->s = s;
+        preprocess();
+    }
+
+    void preprocess() {
+        string t = "";
+        for (auto c : this->s) {
+            t += '#';
+            t += c;
+        }
+        t += "#";
+        this->P = manacher_odd(t);
+    }
+
+    vector<int> getPArray() {
+        return this->P;
+    }
+
+    // 0-based index
+    // Odd: cenIdx
+    // Even (only cenIdx >= 1): cenIdx && cenIdx - 1 
+    int getLongest(int cenIdx, bool odd) {
+        int idx;
+        if (odd) idx = 1 + 2*cenIdx;
+        else idx = 1 + 2*cenIdx - 1;
+        return this->P[idx] - 1;
+    }
+    
+    // 0-based index
+    bool isPalindrome(int l, int r) {
+        return (r-l+1) <= getLongest((l+r+1)/2, l%2==r%2);
+    }
+
+    vector<int> manacher_odd(string s) {
+        int n = s.size();
+        s = "$" + s + "^";
+        vector<int> p(n + 2);
+        int l = 0, r = 2;   
+        for (int i = 1; i <= n; i++) {
+            p[i] = max(0, min(r - i, p[l + r - i]));    
+            while (s[i - p[i]] == s[i + p[i]]) {
+                p[i]++;
+            }
+            if (i + p[i] > r) {
+                l = i - p[i]; r = i + p[i];
+            }
+        }
+        return vector<int>(p.begin() + 1, p.end() - 1);
+    }
+};
+
+int n, q;
+string s;
+vector<vector<int>> prf; // check all same substring
+vector<vector<int>> prf1; // check alternate substring
+bool all_same(ll _l, ll _r) {
+    char c = s[_l];
+    return (prf[c-'a'][_r+1] - prf[c-'a'][_l]) == (_r - _l + 1);
+}
+bool alternate(ll _l, ll _r) {
+    char c1 = s[_l];
+    char c2 = s[_l + 1];
+    int r1 = (_l + _r)&1 ? _r - 1 : _r;
+    int r2 = (_l+1 + _r)&1 ? _r - 1 : _r;
+    int sum1 = prf1[c1-'a'][r1+2] - prf1[c1-'a'][_l];
+    int sum2 = prf1[c2-'a'][r2+2] - prf1[c2-'a'][_l+1];
+    int len = _r - _l + 1;
+    return (sum1 + sum2 == len);
+}
+void solve() {
+    cin >> n >> q >> s;
+    Manacher manacher(s);
+    prf = vector<vector<int>>(26, vector<int>(n + 1, 0));
+    prf1 = vector<vector<int>>(26, vector<int>(n + 2, 0));
+    for (int i = 0; i < n; i++) {
+        prf[s[i] - 'a'][i + 1] = 1;
+        prf1[s[i] - 'a'][i + 2] = 1;
+    }
+    for (int i = 0; i < 26; i++) {
+        for (int j = 1; j <= n; j++) {
+            prf[i][j] = prf[i][j] + prf[i][j - 1];
+        }
+    }
+    for (int i = 0; i < 26; i++) {
+        for (int j = 2; j <= n + 1; j++) {
+            prf1[i][j] = prf1[i][j] + prf1[i][j - 2];
+        }
+    }
+    for (ll i = 0; i < q; i++) {
+        ll _l, _r; cin >> _l >> _r;
+        _l--; _r--;
+        ll len = _r - _l + 1;
+        ll ans = 0;
+        if (len > 1) {
+            if (all_same(_l, _r)) {
+                ans = 0;
+            } else if (alternate(_l, _r)) {
+                ll min = 2;
+                ll max = len&1 ? len - 1 : len - 2;
+                if (min <= len && max >= min) ans = 1LL*(max + min)*((max-min)/2+1)/2;
+                if (!manacher.isPalindrome(_l, _r)) ans += len;
+            } else {
+                ans = 1LL*len*(len+1)/2 - 1 - (manacher.isPalindrome(_l, _r) ? len : 0);
+            }
+        }
+        
+        cout << ans << nline;
+    }
+}
+
+int main() {
+#ifdef ThinhNgo
+    freopen("Error.txt", "w", stderr);
+#endif
+    fastio();
+    IN_OUT();
+    auto start1 = high_resolution_clock::now();
+    int T = 1;
+#ifdef ThinhNgo_use_cases
+    cin >> T;
+#endif
+    pre_compute();
+    while (T--) {
+        solve();
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifdef ThinhNgo
+    cerr << "Time: " << duration . count() / 1000 << " ms" << endl;
+#endif
+}
