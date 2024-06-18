6d9f53efe3acc82cee18afd1c7eb22fb1725f298
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2024-06-18 14:26:59 -0700

    ok

--- atcoder/contests/Educational_DP/M_Candies.cpp
+++ atcoder/contests/Educational_DP/M_Candies.cpp
@@ -1,6 +1,6 @@
 /**
  * Author: Thinh Ngo Ngoc
- * Solution for: https://atcoder.jp/contests/abc148/tasks/abc148_e
+ * Solution for: https://atcoder.jp/contests/dp/tasks/dp_m
 */
 #pragma GCC optimize("O3,unroll-loops")
  
@@ -70,6 +70,7 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 ll __gcd__(ll a, ll b) {if (!a || !b) return a | b; unsigned shift = __builtin_ctz(a | b); a >>= __builtin_ctz(a); do { b >>= __builtin_ctz(b); if (a > b) swap(a, b); b -= a; } while (b); return a << shift;} // only a >= 0 && b >= 0
 ll gcd(ll a, ll b) {a = abs(a); b = abs(b); return __gcd__(a, b);} // get abs(a), abs(b) in case a < 0 || b < 0
 ll expo(ll a, ll b, ll mod) {if (a==0) return 1%mod; ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;} // a = 0 return 0 | b = 0 return 1
+ll expo(ll a, ll b) {ll res = 1; while (b > 0) {if (b & 1)res = res * a; a = a * a; b = b >> 1;} return res;} // a = 0 return 0 | b = 0 return 1
 void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
 ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
 ll mminvprime(ll a, ll b) {assert(gcd(a, b) == 1); return expo(a, b - 2, b);} // inverse mod: a^-1 % b = a^(b-2) % b. When gcd(a,b)=1 (or b prime)
@@ -83,18 +84,40 @@ ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) %
 ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
 ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
 ll getRandomNumber(ll l, ll r) {return uniform_int_distribution<ll>(l, r)(rng);} 
+struct custom_hash {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}size_t operator()(uint64_t x) const {static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x + FIXED_RANDOM);}}; // https://codeforces.com/blog/entry/62393
+struct custom_hash_pair {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}size_t operator()(pair<uint64_t,uint64_t> x) const {static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x.first + FIXED_RANDOM)^(splitmix64(x.second + FIXED_RANDOM) >> 1);}}; // https://codeforces.com/blog/entry/62393
 /*--------------------------------------------------------------------------------------------------------------------------*/
+// #define ThinhNgo_use_cases
 
-ll n;
+
+
+const int mxn = 1e2 + 5;
+const int mxk = 1e5 + 5;
+int n, k;
+int a[mxn];
+int dp[mxn][mxk]; // number of ways to share candies: if only consider first i childrens and total number of candies = j
+void pre_compute() {
+
+}
 void solve() {
-    cin >> n;
-    if (n&1) {cout << 0 << nline; return;}
-    ll ans = 0;
-    n/=2;
-    for (ll cur = 5; cur <= n; cur*=5) {
-        ans += n/cur;
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    for (int j = 0; j <= min(a[1], k); j++) dp[1][j] = 1;
+
+    int lower, cur_window;
+    for (int i = 2; i <= n; i++) {
+        cur_window = 0;
+        for (int j = 0; j <= k; j++) {
+            lower = j - min(a[i], j); // lower keep equal to 0 until window size > a[i] + 1 then lower increase (upper always j)
+            cur_window = (cur_window + dp[i - 1][j]) % MOD;
+            if (lower > 0) {
+                cur_window = (cur_window - dp[i - 1][lower - 1]) % MOD;
+                if (cur_window < 0) cur_window += MOD;
+            }
+            dp[i][j] = cur_window;
+        }
     }
-    cout << ans << nline;
+    cout << dp[n][k] << nline;
 }
 
 int main() {
@@ -104,7 +127,14 @@ int main() {
     fastio();
     IN_OUT();
     auto start1 = high_resolution_clock::now();
-    solve();
+    int T = 1;
+#ifdef ThinhNgo_use_cases
+    cin >> T;
+#endif
+    pre_compute();
+    while (T--) {
+        solve();
+    }
     auto stop1 = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop1 - start1);
 #ifdef ThinhNgo
