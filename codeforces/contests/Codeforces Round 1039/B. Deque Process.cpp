adc3d1915db8452c88e01f30f924cf669d9cb66d
ngongocthinh159 <ngongocthinh159@gmail.com>
2025-08-03 12:50:36 +0700

    ok

---
+++ codeforces/contests/Codeforces Round 1039/B. Deque Process.cpp
@@ -0,0 +1,106 @@
+/**
+ * Author: Thinh Ngo Ngoc
+ * Solution for: https://codeforces.com/contest/2128/problem/B
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define nline '\n'
+#define ll long long
+#define MOD 1000000007
+#define MOD1 998244353
+#define LINF ((long long)1e18)
+#define IINF ((int)1e9)
+#define MASK(i) (1LL << (i))
+#define all(v) (v).begin(), (v).end()
+
+
+template<class X, class Y>
+bool minimize(X &x, const Y &y) {
+    if (x > y) {
+        x = y;
+        return true;
+    } else return false;
+}
+template<class X, class Y>
+bool maximize(X &x, const Y &y) {
+    if (x < y) {
+        x = y;
+        return true;
+    } else return false;
+}
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+void IN_OUT() {
+#ifndef ONLINE_JUDGE
+freopen("Input.txt", "r", stdin);
+freopen("Output.txt", "w", stdout);
+#endif
+}
+struct chash {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}size_t operator()(uint64_t x) const {static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x + FIXED_RANDOM);}}; // https://codeforces.com/blog/entry/62393
+struct chashp {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}size_t operator()(pair<uint64_t,uint64_t> x) const {static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x.first + FIXED_RANDOM)^(splitmix64(x.second + FIXED_RANDOM) >> 1);}}; // https://codeforces.com/blog/entry/62393
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+#define N 100005
+int n;
+int a[N];
+void pre_compute() {
+    
+}
+void solve() {
+    cin >> n;
+    deque<int> q;
+    for (int i = 0; i < n; i++){
+        cin >> a[i];
+        q.push_back(a[i]);
+    }
+    string s = "";
+    int turn = 0;
+    vector<int> cur;
+    while (n--) {
+        int x;
+        if (turn == 0) {
+            x = q.front();
+            q.pop_front();
+            s += 'L';
+        } else {
+            x = q.back();
+            q.pop_back();
+            s += 'R';
+        }
+        cur.push_back(x);
+        if (cur.size() >= 5) {
+            bool ok = true;
+            int j = cur.size() - 1;
+            if (cur[j] > cur[j - 1] && cur[j - 1] > cur[j - 2] && cur[j - 2] > cur[j - 3] && cur[j - 3] > cur[j - 4])
+                ok = false;
+            if (cur[j] < cur[j - 1] && cur[j - 1] < cur[j - 2] && cur[j - 2] < cur[j - 3] && cur[j - 3] < cur[j - 4])
+                ok = false;
+            if (!ok) {
+                swap(s[s.size() - 1], s[s.size() - 2]);
+                swap(cur[cur.size() - 1], cur[cur.size() - 2]);
+            } else {
+                turn ^= 1;
+            }
+        } else {
+            turn ^= 1;
+        }
+    }
+    cout << s << nline;
+    // for (auto x : cur) cout << x << ' ';
+    // cout << nline;
+}
+
+int main() {
+    fastio();
+    IN_OUT();
+    int T = 1;
+    cin >> T;
+    pre_compute();
+    for (int cases = 1; cases <= T; cases++) {
+
+        solve();
+    }
+    return 0;
+}
