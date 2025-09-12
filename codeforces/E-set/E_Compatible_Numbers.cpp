8d1ce9036f1f5903b0c28ba6487c0db9a7899121
ngongocthinh159 <ngongocthinh124@gmail.com>
2025-09-12 12:45:12 +0700

    ok

---
+++ codeforces/E-set/E_Compatible_Numbers.cpp
@@ -0,0 +1,80 @@
+/**
+ * Author: Thinh Ngo Ngoc
+ * Solution for: https://codeforces.com/contest/165/problem/E
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define nline '\n'
+#define ll long long
+#define MOD 1000000007
+#define MOD1 998244353
+#define LINF ((long long)1e18)
+#define IINF ((int)1e9)
+#define MASK(i) (1LL << (i))
+#define all(v) (v).begin(), (v).end()
+
+
+template<class X, class Y>
+bool minimize(X &x, const Y &y) {
+    if (x > y) {
+        x = y;
+        return true;
+    } else return false;
+}
+template<class X, class Y>
+bool maximize(X &x, const Y &y) {
+    if (x < y) {
+        x = y;
+        return true;
+    } else return false;
+}
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+void IN_OUT() {
+#ifndef ONLINE_JUDGE
+freopen("Input.txt", "r", stdin);
+freopen("Output.txt", "w", stdout);
+#endif
+}
+struct chash {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}size_t operator()(uint64_t x) const {static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x + FIXED_RANDOM);}}; // https://codeforces.com/blog/entry/62393
+struct chashp {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}size_t operator()(pair<uint64_t,uint64_t> x) const {static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x.first + FIXED_RANDOM)^(splitmix64(x.second + FIXED_RANDOM) >> 1);}}; // https://codeforces.com/blog/entry/62393
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+#define BIT 22
+#define N 1000005
+int n, a[N], dp[1 << 22];
+void pre_compute() {
+    
+}
+void solve() {
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> a[i];
+	for (int i = 0; i < n; i++) dp[a[i]] = a[i];
+	for (int i = 0; i < BIT; i++)
+		for (int state = 0; state < (1 << BIT); state++) {
+			if ((state >> i) & 1)
+				dp[state] = max(dp[state], dp[state ^ (1 << i)]);
+		}
+	for (int i = 0; i < n; i++) {
+		int ra = a[i] ^ ((1 << BIT) - 1);
+		if (dp[ra]) 
+			cout << dp[ra] << ' ';
+		else
+			cout << -1 << ' ';
+	}
+}
+
+int main() {
+    fastio();
+    IN_OUT();
+    int T = 1;
+    // cin >> T;
+    pre_compute();
+    for (int cases = 1; cases <= T; cases++) {
+
+        solve();
+    }
+    return 0;
+}
