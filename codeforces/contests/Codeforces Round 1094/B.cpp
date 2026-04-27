0c89509db4a6f72ba071bd549555435dcb04f4cd
Thinh Ngo <ngongocthinh124@gmail.com>
2026-04-27 08:25:00 +0700

    ok

---
+++ codeforces/contests/Codeforces Round 1094/B.cpp
@@ -0,0 +1,100 @@
+/**
+ * Author: Thinh Ngo Ngoc
+ * Solution for: https://codeforces.com/contest/2222/problem/B
+*/
+
+// #include<bits/stdc++.h>
+#include "bits/stdc++.h"
+using namespace std;
+
+#define nline '\n'
+#define ll long long
+#define MOD 1000000007
+#define MOD1 998244353
+#define LINF ((long long)1e18)
+#define IINF ((int)1e9)
+#define MASK(i) (1LL << (i))
+#define all(v) (v).begin(), (v).end()
+
+
+template<class X, class Y>
+bool minimize(X &x, const Y &y) {
+    if (x > y) {
+        x = y;
+        return true;
+    } else return false;
+}
+template<class X, class Y>
+bool maximize(X &x, const Y &y) {
+    if (x < y) {
+        x = y;
+        return true;
+    } else return false;
+}
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+void IN_OUT() {
+#ifndef ONLINE_JUDGE
+freopen("Input.txt", "r", stdin);
+freopen("Output.txt", "w", stdout);
+#endif
+}
+struct chash {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}size_t operator()(uint64_t x) const {static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x + FIXED_RANDOM);}}; // https://codeforces.com/blog/entry/62393
+struct chashp {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}size_t operator()(pair<uint64_t,uint64_t> x) const {static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x.first + FIXED_RANDOM)^(splitmix64(x.second + FIXED_RANDOM) >> 1);}}; // https://codeforces.com/blog/entry/62393
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+
+void pre_compute() {
+    
+}
+void solve() {
+    int n, m; cin >> n >> m;
+    int a[n + 1], x[m + 1];
+    int o = 0, e = 0;
+    vector<int> odds, evens;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        if (i&1) odds.push_back(a[i]);
+        else evens.push_back(a[i]);
+    }
+    for (int i = 1; i <= m; i++) {
+        cin >> x[i];
+        if (x[i] & 1) o++;
+        else e++;
+    }
+    sort(all(odds));
+    sort(all(evens));
+    if (o) {
+        odds.pop_back();
+        o--;
+    }
+    if (e) {
+        evens.pop_back();
+        e--;
+    }
+    while (o && odds.size() && odds.back() > 0) {
+        o--;
+        odds.pop_back();
+    }
+    while (e && evens.size() && evens.back() > 0) {
+        e--;
+        evens.pop_back();
+    }
+    ll sum = 0;
+    for (auto  x : odds) sum += x;
+    for (auto  x : evens) sum += x;
+    cout << sum << nline;
+}
+
+int main() {
+    fastio();
+    IN_OUT();
+    int T = 1;
+    cin >> T;
+    pre_compute();
+    for (int cases = 1; cases <= T; cases++) {
+
+        solve();
+    }
+    return 0;
+}
---
+++ codeforces/contests/Codeforces Round 1094/C.cpp
@@ -0,0 +1,84 @@
+/**
+ * Author: Thinh Ngo Ngoc
+ * Solution for: https://codeforces.com/contest/2222/problem/C
+*/
+
+// #include<bits/stdc++.h>
+#include "bits/stdc++.h"
+using namespace std;
+
+#define nline '\n'
+#define ll long long
+#define MOD 1000000007
+#define MOD1 998244353
+#define LINF ((long long)1e18)
+#define IINF ((int)1e9)
+#define MASK(i) (1LL << (i))
+#define all(v) (v).begin(), (v).end()
+
+
+template<class X, class Y>
+bool minimize(X &x, const Y &y) {
+    if (x > y) {
+        x = y;
+        return true;
+    } else return false;
+}
+template<class X, class Y>
+bool maximize(X &x, const Y &y) {
+    if (x < y) {
+        x = y;
+        return true;
+    } else return false;
+}
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+void IN_OUT() {
+#ifndef ONLINE_JUDGE
+freopen("Input.txt", "r", stdin);
+freopen("Output.txt", "w", stdout);
+#endif
+}
+struct chash {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}size_t operator()(uint64_t x) const {static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x + FIXED_RANDOM);}}; // https://codeforces.com/blog/entry/62393
+struct chashp {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}size_t operator()(pair<uint64_t,uint64_t> x) const {static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x.first + FIXED_RANDOM)^(splitmix64(x.second + FIXED_RANDOM) >> 1);}}; // https://codeforces.com/blog/entry/62393
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+// if we divide an array into k subarray with the same median y, then y = global median of whole array
+void pre_compute() {
+    
+}
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    vector<int> b = a;
+    sort(all(b));
+    int med = b[n/2+1];
+    vector<int> dp(n + 1, -IINF);
+    dp[0] = 0;
+    for (int i = 1; i <= n; i++) {
+        int x = 0, y = 0, z = 0;
+        for (int j = i; j >= 1; j--) {
+            if (a[j] < med) x++;
+            else if (a[j] == med) y++;
+            else z++;
+            if ((i - j + 1) & 1) {
+                if (x + y > z && y + z > x) dp[i] = max(dp[i], 1 + dp[j - 1]);
+            }
+        }
+    }
+    cout << dp[n] << nline;
+}
+
+int main() {
+    fastio();
+    IN_OUT();
+    int T = 1;
+    cin >> T;
+    pre_compute();
+    for (int cases = 1; cases <= T; cases++) {
+
+        solve();
+    }
+    return 0;
+}
