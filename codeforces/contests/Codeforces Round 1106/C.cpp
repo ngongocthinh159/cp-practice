6e37b67b13e55a6bf71a413c73572fc3c4b047e6
Thinh Ngo <ngongocthinh124@gmail.com>
2026-06-30 12:09:33 +0700

    ok

---
+++ codeforces/contests/Codeforces Round 1106/C.cpp
@@ -0,0 +1,99 @@
+/**
+ * Author: Thinh Ngo Ngoc
+ * Solution for: https://codeforces.com/contest/2238/problem/C
+*/
+
+#include "bits/stdc++.h"
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define nline '\n'
+#define ll long long
+#define MOD 1000000007
+#define MOD1 998244353
+#define LINF ((long long)1e18)
+#define IINF ((int)1e9)
+#define MASK(i) (1LL << (i))
+#define all(v) (v).begin(), (v).end()
+
+
+template<class X, class Y>
+bool minimize(X &x, const Y &y) {
+    if (x > y) {
+        x = y;
+        return true;
+    } else return false;
+}
+template<class X, class Y>
+bool maximize(X &x, const Y &y) {
+    if (x < y) {
+        x = y;
+        return true;
+    } else return false;
+}
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+void IN_OUT() {
+#ifndef ONLINE_JUDGE
+freopen("Input.txt", "r", stdin);
+freopen("Output.txt", "w", stdout);
+#endif
+}
+struct chash {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}size_t operator()(uint64_t x) const {static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x + FIXED_RANDOM);}}; // https://codeforces.com/blog/entry/62393
+struct chashp {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}size_t operator()(pair<uint64_t,uint64_t> x) const {static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x.first + FIXED_RANDOM)^(splitmix64(x.second + FIXED_RANDOM) >> 1);}}; // https://codeforces.com/blog/entry/62393
+ll expo(ll a, ll b, ll mod = MOD) {if (a==0) return 1%mod; ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;} // a = 0 return 0 | b = 0 return 1
+ll __gcd__(ll a, ll b) {if (!a || !b) return a | b; unsigned shift = __builtin_ctz(a | b); a >>= __builtin_ctz(a); do { b >>= __builtin_ctz(b); if (a > b) swap(a, b); b -= a; } while (b); return a << shift;} // only a >= 0 && b >= 0
+ll gcd(ll a, ll b) {a = abs(a); b = abs(b); return __gcd__(a, b);} // get abs(a), abs(b) in case a < 0 || b < 0
+typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+#define N 200005
+vector<int> g[N];
+ll ans = 0;
+void pre_compute() {
+    
+}
+int dfs(int u, int p, int d) {
+    int mx1 = -1, mx2 = -1, cnt = 0;
+    ans++;
+    for (auto v : g[u]) if (v != p) {
+        cnt++;
+        int cmax = dfs(v, u, d + 1);
+        if (cmax > mx1) {
+            mx2 = mx1;
+            mx1 = cmax;
+        } else if (cmax > mx2) {
+            mx2 = cmax;
+        }
+    }
+    if (cnt >= 2) ans += mx2 - d;
+    return cnt == 0 ? d : mx1;
+}
+void solve() {
+    int n; cin >> n;
+    for (int i = 0; i <= n; i++) g[i].clear();
+    for (int u = 2; u <= n; u++) {
+        int pu; cin >> pu;
+        g[u].push_back(pu);
+        g[pu].push_back(u);
+    }
+    ans = 0;
+    dfs(1, 0, 0);
+    cout << ans << nline;
+}
+
+int main() {
+    fastio();
+    IN_OUT();
+    int T = 1;
+    cin >> T;
+    pre_compute();
+    for (int cases = 1; cases <= T; cases++) {
+
+        solve();
+    }
+    return 0;
+}
---
+++ codeforces/contests/Codeforces Round 1106/D.cpp
@@ -0,0 +1,83 @@
+/**
+ * Author: Thinh Ngo Ngoc
+ * Solution for: https://codeforces.com/contest/2238/problem/D
+*/
+
+#include "bits/stdc++.h"
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define nline '\n'
+#define ll long long
+#define MOD 1000000007
+#define MOD1 998244353
+#define LINF ((long long)1e18)
+#define IINF ((int)1e9)
+#define MASK(i) (1LL << (i))
+#define all(v) (v).begin(), (v).end()
+
+
+template<class X, class Y>
+bool minimize(X &x, const Y &y) {
+    if (x > y) {
+        x = y;
+        return true;
+    } else return false;
+}
+template<class X, class Y>
+bool maximize(X &x, const Y &y) {
+    if (x < y) {
+        x = y;
+        return true;
+    } else return false;
+}
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+void IN_OUT() {
+#ifndef ONLINE_JUDGE
+freopen("Input.txt", "r", stdin);
+freopen("Output.txt", "w", stdout);
+#endif
+}
+struct chash {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}size_t operator()(uint64_t x) const {static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x + FIXED_RANDOM);}}; // https://codeforces.com/blog/entry/62393
+struct chashp {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}size_t operator()(pair<uint64_t,uint64_t> x) const {static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x.first + FIXED_RANDOM)^(splitmix64(x.second + FIXED_RANDOM) >> 1);}}; // https://codeforces.com/blog/entry/62393
+ll expo(ll a, ll b, ll mod = MOD) {if (a==0) return 1%mod; ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;} // a = 0 return 0 | b = 0 return 1
+ll __gcd__(ll a, ll b) {if (!a || !b) return a | b; unsigned shift = __builtin_ctz(a | b); a >>= __builtin_ctz(a); do { b >>= __builtin_ctz(b); if (a > b) swap(a, b); b -= a; } while (b); return a << shift;} // only a >= 0 && b >= 0
+ll gcd(ll a, ll b) {a = abs(a); b = abs(b); return __gcd__(a, b);} // get abs(a), abs(b) in case a < 0 || b < 0
+typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+#define N 1000005
+int spf[N];
+void pre_compute() {
+    for (int i = 0; i < N; i++) spf[i] = i;
+    for (int i = 2; i * i < N; i++) if (spf[i] == i)
+        for (int j = i * i; j < N; j += i) spf[j] = min(spf[j], i);
+}
+void solve() {
+    int n; cin >> n;
+    int power = 0, prime = 0;
+    while (n != 1) {
+        int fact = spf[n], c = 0;
+        while (n % fact == 0) n /= fact, c++;
+        power += c;
+        prime++;
+    }
+    cout << power + prime - 1 << nline;
+}
+
+int main() {
+    fastio();
+    IN_OUT();
+    int T = 1;
+    cin >> T;
+    pre_compute();
+    for (int cases = 1; cases <= T; cases++) {
+
+        solve();
+    }
+    return 0;
+}
